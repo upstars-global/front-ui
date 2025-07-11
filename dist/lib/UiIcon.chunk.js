3439e5fe294569c01c5fe24c5b7896676ce9b666
ikhartov <i.khartov@upstars.com>
2025-07-11 11:34:16 +0300

    chore: build

--- dist/lib/UiIcon.chunk.js
+++ dist/lib/UiIcon.chunk.js
@@ -1,15 +1,17 @@
-import { defineComponent as u, computed as n, createElementBlock as i, openBlock as r, normalizeClass as m, mergeProps as h } from "vue";
-import { icons as p } from "../components/icon/icons.js";
-const _ = ["innerHTML"], f = ["src"], z = /* @__PURE__ */ u({
+import { defineComponent as _, useAttrs as v, computed as r, createElementBlock as a, openBlock as i, mergeProps as u } from "vue";
+import { twMerge as w } from "tailwind-merge";
+import { icons as d } from "../components/icon/icons.js";
+const y = ["innerHTML"], S = ["src"], g = /* @__PURE__ */ _({
   name: "UiIcon",
+  inheritAttrs: !1,
   __name: "UiIcon",
   props: {
     name: {},
     src: {},
     size: { default: "24" }
   },
-  setup(t) {
-    const l = {
+  setup(m) {
+    const f = {
       16: "w-4 h-4",
       20: "w-5 h-5",
       24: "w-6 h-6",
@@ -17,21 +19,26 @@ const _ = ["innerHTML"], f = ["src"], z = /* @__PURE__ */ u({
       40: "w-10 h-10",
       48: "w-12 h-12",
       full: "w-full h-full"
-    }, e = t, s = n(() => {
-      if (e.name)
-        return p[e.name];
-    }), a = n(() => ({ height: e.size, width: e.size })), o = n(() => `ui-icon ${l[e.size]}`);
-    return (c, w) => s.value && !c.src ? (r(), i("div", {
+    }, t = m, s = v(), n = r(() => {
+      if (t.name)
+        return d[t.name];
+    }), h = r(() => ({ height: t.size, width: t.size })), c = r(() => {
+      const e = { ...s };
+      return delete e.class, e;
+    }), p = r(() => {
+      let e = "";
+      return s.class ? (Array.isArray(s.class) ? e = s.class.join(" ") : typeof s.class == "object" && (e = Object.keys(s.class).filter((l) => s.class[l]).join(" ")), e || s.class) : e;
+    }), o = r(() => w("ui-icon", f[t.size], p.value));
+    return (e, l) => n.value && !e.src ? (i(), a("div", u({
       key: 0,
-      class: m(o.value),
-      innerHTML: s.value
-    }, null, 10, _)) : (r(), i("img", h({ key: 1 }, a.value, {
+      class: o.value
+    }, c.value, { innerHTML: n.value }), null, 16, y)) : (i(), a("img", u({ key: 1 }, { ...c.value, ...h.value }, {
       class: o.value,
-      src: c.src || void 0,
+      src: e.src || void 0,
       alt: "icon"
-    }), null, 16, f));
+    }), null, 16, S));
   }
 });
 export {
-  z as _
+  g as _
 };
